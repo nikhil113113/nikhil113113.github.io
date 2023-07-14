796b20e52f0c647cf647febe004a224fd93787ef
nikhil113113 <132265713+nikhil113113@users.noreply.github.com>
2023-07-14 17:26:36 +0530

    hi

---
+++ js/owl-carousal.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        800:{
+            items:2
+        },
+        1100:{
+            items:3
+        }
+    } 
+
+})
+
+ 
