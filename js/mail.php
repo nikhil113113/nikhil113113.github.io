ede6758daf8a85d866e77b977b9595b5d7a89abc
nikhil113113 <132265713+nikhil113113@users.noreply.github.com>
2023-07-15 15:10:25 +0530

    nk

--- js/mail.php
+++ js/mail.php
@@ -2,17 +2,17 @@
 //get data from form  
 
 $name = $_POST['name'];
-$email= $_POST['mail'];
-$subject= $_POST['subject'];
-$message= $_POST['message'];
+$mail = $_POST['mail'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
 // $number= $_POST['number'];
 
 $to = "nikhilmanojn3@gmail.com";
 $subject = "Mail From Rinshad";
-$txt ="Name = ". $name . "\r\n  Email = " . $mail . "\r\n Message =" . $message . "\r\n Subject =" . $subject ;
+$txt = "Name = ". $name . "\r\n  Email = " . $mail . "\r\n Message =" . $message . "\r\n Subject =" . $subject;
 $headers = "From: nikhilmanojn3@gmail.com" . "\r\n" .
 "CC: nikhilmanojn3@gmail.com";
-if($email!=NULL){
+if($mail!=NULL){
     mail($to,$subject,$txt,$headers);
 }
 //redirect
